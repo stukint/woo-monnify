a94cde3ed15f60793b7b54980ad2098b9fcf5077
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2025-01-17 19:02:54 +0000

    api setting toggle

--- assets/images/js/monnify-admin.js
+++ assets/images/js/monnify-admin.js
@@ -11,6 +11,32 @@ jQuery( function( $ ) {
 		init: function() {
 
             //Toggle api key setting.
+            $( document.body ).on('change', '#woocommerce_monnify_testmode', function() {
+                var test_secret_key = $( '#woocommerce_monnify_test_secret_key' ).parents( 'tr' ).eq( 0 ),
+                    test_api_key = $( '#woocommerce_monnify_test_api_key' ).parents( 'tr' ).eq( 0 ),
+                    test_contract_code = $( '#woocommerce_monnify_test_contract_code' ).parents( 'tr' ).eq( 0 ),
+                    live_secret_key = $( '#woocommerce_monnify_live_secret_key' ).parents( 'tr' ).eq( 0 ),
+                    live_api_key = $( '#woocommerce_monnify_live_api_key' ).parents( 'tr' ).eq( 0 ),
+                    live_contract_code = $( '#woocommerce_monnify_live_contract_code' ).parents( 'tr' ).eq( 0 );
+
+                    if ( $( this ).is( ':checked' ) ) {
+                        test_secret_key.show();
+                        test_api_key.show();
+                        test_contract_code.show();
+                        live_secret_key.hide();
+                        live_api_key.hide();
+                        live_contract_code.hide();
+                    } else {
+                        test_secret_key.hide();
+                        test_api_key.hide();
+                        test_contract_code.hide();
+                        live_secret_key.show();
+                        live_api_key.show();
+                        live_contract_code.show();
+                    }
+            });
+
+            $( '#woocommerce_monnify_testmode' ).change();
 
             //Toogle bank filter setting.
 
