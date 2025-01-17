20d7fe3e40363ff22df07d0276898b3e1304a0a2
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2025-01-17 18:08:24 +0000

    Settings Options

---
+++ assets/images/js/monnify-admin.js
@@ -0,0 +1,24 @@
+jQuery( function( $ ) {
+    'use strict';
+
+	/**
+	 * Object to handle Monnify admin functions.
+	 */
+	var wc_monnify_admin = {
+        /**
+		 * Initialize.
+		 */
+		init: function() {
+
+            //Toggle api key setting.
+
+            //Toogle bank filter setting.
+
+            //Toogle secret
+            
+        }
+    }
+
+    wc_monnify_admin.init();
+
+});
