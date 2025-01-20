a475dc0cc77486292b6e51de9f4e5de30f6524da
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2025-01-20 18:55:41 +0000

    more debugging

--- includes/class-wc-gateway-monnify.php
+++ includes/class-wc-gateway-monnify.php
@@ -739,20 +739,13 @@ class WC_Gateway_Monnify extends WC_Payment_Gateway_CC {
 		
 		if ( isset( $_REQUEST['monnify_txnref'] ) ) {
 			$monnify_txn_ref = sanitize_text_field( $_REQUEST['monnify_txnref'] );
-		} elseif ( isset( $_REQUEST['transactionReference'] ) ) {
-			$monnify_txn_ref = sanitize_text_field( $_REQUEST['transactionReference'] );
+		} elseif ( isset( $_REQUEST['paymentReference'] ) ) {
+			$monnify_txn_ref = sanitize_text_field( $_REQUEST['paymentReference'] );
 		} elseif ( isset( $_GET['paymentReference'] ) ){
 			$monnify_txn_ref = sanitize_text_field( $_GET['paymentReference'] );
 		}else {
 			$monnify_txn_ref = false;
 		}
-
-		error_log(print_r('monnify_txnref is: ' . $_REQUEST['monnify_txnref'], true));
-		error_log(print_r('transactionReference is: ' . $_REQUEST['transactionReference'], true));
-		error_log(print_r('request paymentReference is: ' . $_REQUEST['paymentReference'], true));
-		error_log(print_r('get paymentReference is: ' . $_GET['paymentReference'], true));
-		error_log(print_r('Server is: ' . $_SERVER, true));
-		
 		
 		@ob_clean();
 
